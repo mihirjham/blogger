88dc607118a162956348b98bab4b19f9bfbdd23b
Mihir Jham <mihirjham@gmail.com>
2015-08-18 03:58:36 +0000

    Tagging functionality implemented.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ActiveRecord::Base
     has_many :comments;
+    has_many :taggings;
+    has_many :tags, through: :taggings;
+    
+    def tag_list
+        self.tags.collect do |tag|
+            tag.name;
+        end.join(", ");
+    end
+    
+    def tag_list=(str)
+        str = str.split(",");
+        str = str.map {|element| element.strip.downcase }.uniq;
+        new_tags = str.collect { |element| Tag.find_or_create_by(name: element) };
+        self.tags = new_tags;
+    end
 end
